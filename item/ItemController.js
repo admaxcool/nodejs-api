efcae47367097b48804454b346beb8fc4a821ab9
Yung Chun Ern Martin <martinyce@gmail.com>
2017-09-27 23:45:27 +0800

    get latest value according to key

--- item/ItemController.js
+++ item/ItemController.js
@@ -17,3 +17,13 @@ router.post('/', (req, res) => {
 		res.status(200).send(item);
 	});
 });
+
+// Get the latest value according to key
+router.get('/:key', (req, res) => {
+	Item.find({ 'key': req.params.key }, (err, item) => {
+		if (err) return res.status(500).send('error');
+		if (item.length === 0) return res.status(404).send('not found');
+		console.log(item)
+		res.status(200).send('value: ' + item[0].value);
+	}).sort({timestamp: -1});
+}); 
