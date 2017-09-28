35aea9967520c7480bb1dc0c5f781b8ea57fe5ed
Yung Chun Ern Martin <martinyce@gmail.com>
2017-09-28 23:18:26 +0800

    add get list of all items

--- item/ItemController.js
+++ item/ItemController.js
@@ -6,6 +6,14 @@ router.use(bodyParser.urlencoded({ extended: true }));
 var Item = require('./Item');
 module.exports = router;
 
+// Get list of all items
+router.get('/', (req, res) => {
+	Item.find({}, (err, items) => {
+		if (err) return res.status(500).send('error');
+		res.status(200).send(items);
+	});
+});
+
 // Create an item or update existing item
 router.post('/', (req, res) => {
 	var query = { 'key': req.body.key };
@@ -16,7 +24,7 @@ router.post('/', (req, res) => {
 	var options = {
 		upsert: true,
 		new: true
-	}
+	};
 	Item.findOneAndUpdate(query, update, options, (err, item) => {
 		if (err) return res.status(500).send('error');
 		res.status(200).send(item);
@@ -25,7 +33,7 @@ router.post('/', (req, res) => {
 
 // Get the value according to key
 router.get('/:key', (req, res) => {
-	var timestampQuery = parseInt(req.query.timestamp)
+	var timestampQuery = parseInt(req.query.timestamp);
 	// Get the value less than or equal to the timestamp in query string
 	if (timestampQuery) {
 		Item.find({ 
@@ -40,7 +48,7 @@ router.get('/:key', (req, res) => {
 			res.status(200).send('value: ' + item[0].value)
 		});
 	}
-	// Get the latest value accroding to key when no timestamp query string is given
+	// Get the latest value according to key when no timestamp query string is given
 	else {
 		Item.find({ 'key': req.params.key }).
 		sort({ 'timestamp': -1 }).
