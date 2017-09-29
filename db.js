b83dea64e0eea5706fb3944edcc5c15a41a01037
Yung Chun Ern Martin <martinyce@gmail.com>
2017-09-29 16:58:00 +0800

    add test and update readme

--- db.js
+++ db.js
@@ -1,4 +1,9 @@
 var mongoose = require('mongoose');
 let DBUSER = process.env.DBUSER
 let DBPASSWORD = process.env.DBPASSWORD
-mongoose.connect(`mongodb://${DBUSER}:${DBPASSWORD}@ds151014.mlab.com:51014/vdragon`)
+
+if (process.env.NODE_ENV === 'test') {
+	mongoose.connect(process.env.DB_TEST_URL)
+} else {
+	mongoose.connect(process.env.DB_URL)
+}
--- server.js
+++ server.js
@@ -4,3 +4,5 @@ var port = process.env.PORT || 8080;
 var server = app.listen(port, () => {
 	console.log('Express server listening on port ' + port);
 });
+
+module.exports = server
